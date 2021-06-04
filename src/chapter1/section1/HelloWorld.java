e7e44ff7511d59d7fed78d1dc70bdd3d595bf37e
sunshineForever2020 <wangyonghuizs@icloud.com>
2021-06-04 17:12:59 +0800

    配置工程， 添加algs4库， 配置application应用

--- src/chapter1/section1/HelloWorld.java
+++ src/chapter1/section1/HelloWorld.java
@@ -1,9 +1,11 @@
 package chapter1.section1;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class HelloWorld {
-    private static void main(String[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        StdOut.println("Hello, world!");
     }
 }
 
